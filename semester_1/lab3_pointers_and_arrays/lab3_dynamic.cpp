8fad04bf639d27b99f6a416939a47c71d4435aaf
AnotherDev12 <antonbychkovsk@gmail.com>
2025-11-05 10:25:23 +0300

    Update lab3_dynamic.cpp

--- semester_1/lab3_pointers_and_arrays/lab3_dynamic.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3_dynamic.cpp
@@ -23,21 +23,21 @@ void inputFromKeyboard(double* arr, int n) {
 }
 
 void fillWithRandom(double* arr, int n) {
-    double a, b;
-    std::cout << "Enter interval boundaries [a, b]: ";
-    if (!(std::cin >> a >> b)) {
+    double lower, upper;
+    std::cout << "Enter interval boundaries [lower, upper]: ";
+    if (!(std::cin >> lower >> upper)) {
         std::cout << "Input error!";
         std::exit(404);
     }
-
-    if (a > b) {
-        std::swap(a, b);
+ 
+    if (lower > upper) {
+        std::swap(lower, upper);
     }
 
     std::mt19937 gen(time(0));
-    std::uniform_real_distribution<double> dist(a, b);
+    std::uniform_real_distribution<double> dist(lower, upper);
 
-    std::cout << "Filling the array with random numbers from thr interval [" << a << ", " << b << "]:\n";
+    std::cout << "Filling the array with random numbers from thr interval [" << lower << ", " << upper << "]:\n";
     for (int i = 0; i < n; i++) {
         arr[i] = dist(gen);
     }
